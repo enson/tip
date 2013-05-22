2bb9cbefbee485f598fec8ea2858f2ff0f693a30
拔赤 <jay.li@alibaba-inc.com>
2013-05-22 17:00:00 +0800

    bugfix

--- 1.0/build/index.js
+++ 1.0/build/index.js
@@ -1,9 +1,77 @@
 /*
 combined files : 
 
+gallery/tip/1.0/node-extension
 gallery/tip/1.0/index
 
 */
+
+
+
+KISSY.add('gallery/tip/1.0/node-extension',function(S,Node,DOM){
+
+	"use strict";
+
+	S.augment(S.Node,{
+
+		beyondViewPortRegion:function(padding){
+			var self = this;
+			var p;
+
+			if(S.isUndefined(padding)){
+				p = 0;
+			}else{
+				p = padding;
+			}
+
+			var vh,vw,left,top;
+
+			var elemOffset = self.offset();
+
+			vh = DOM.viewportHeight();
+			vw = DOM.viewportWidth();
+			left = DOM.scrollLeft();
+			top = DOM.scrollTop();
+
+			function pointInViewPort(x,y){
+				if(	( x >= left + p && x <= (left + vw - p) ) && 
+					( y >= top + p && y <= (top + vh - p) ) ){
+						return true;
+				} else {
+					return false;
+				}
+
+			}
+
+			return {
+				start:pointInViewPort(elemOffset.left,elemOffset.top),
+				end:pointInViewPort(elemOffset.left + self.width(),elemOffset.top + self.height() )
+			};
+
+		},
+
+		// padding：viewport之内的缩几个像素
+		inViewportRegion:function(padding){
+			var self = this;
+
+			var r = self.beyondViewPortRegion(padding);
+
+			if(r.start && r.end ){
+				return true;
+			}else{
+				return false;
+			}
+
+		}
+
+	});
+
+},{
+	requires:[
+		'node','dom'
+	]	
+});
+
 /**
  * @file index.js
  * @brief 
@@ -666,7 +734,7 @@ KISSY.add('gallery/tip/1.0/index',function(S){
 	return Tip;
 	
 },{
-	requires:['overlay'/*'zoo/base/'*/]	
+	requires:['overlay','./node-extension']	
 });
 
 
